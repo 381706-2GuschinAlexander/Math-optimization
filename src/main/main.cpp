e532cfdb74bbb5b68a44412bf16d480eb53feb5a
igush <igush@DESKTOP-3OTTB5S>
2021-09-28 20:29:34 +0300

    changed cmake file

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,24 @@
+#include <cmath>
+#include <iostream>
+
 #include "matplotlibcpp.h"
+#include "fparser.hh"
+
+namespace plt = matplotlibcpp;
+
+float func(float x) {
+  return x + sin(x);
+}
+
 
 
 int main(int argc, char** argv){
+  FunctionParser fp;
+  fp.Parse("x+sin(x)", "x");
+  double X = 10;
+  std::cout << fp.Eval(&X) << std::endl;
+  
+  plt::plot({ 1,3,2,4 });
+  plt::show();
   return 0;
 }
