e13f644e57633d59b72a7a2036840018ca0e5028
381706-2GuschinAlexander <igushin1999@gmail.com>
2021-10-12 14:27:48 +0300

    added plot range

--- src/mathlib/mathlib.cpp
+++ src/mathlib/mathlib.cpp
@@ -1,6 +1,12 @@
 #include "mathlib.h"
 
-FunctionContainer::FunctionContainer(const std::vector<std::string> &str_func_vector, const std::string &str_arg)
+
+double optf::StronginMethod(FunctionParser& parser, double x0, double x1, double L){
+  FunctionParser local_parser = parser;
+  local_parser.ForceDeepCopy();
+}
+
+FunctionContainer::FunctionContainer(const std::vector<std::string> &str_func_vector, const std::string &str_arg, std::vector<std::pair<double,double>>& range) :  range_vec(range)
 {
   for (auto &str_func : str_func_vector)
   {
@@ -14,15 +20,22 @@ void FunctionContainer::Convolution(
   //const std::vector<float> &conv_arg
   )
 {
-  std::vector<double> X(5000);
-  std::vector<double> Y(5000);
-  double x0 = -1;
+  
+  double x0 = std::min(range_vec[0].first, range_vec[0].second);
+  double x1 = std::max(range_vec[0].first, range_vec[0].second);
   double h = 0.005;
-  for(int i =0; i < 5000; ++i)
-  {
+  int num_of_iter = (x1 - x0) / h;
+  std::vector<double> X(num_of_iter);
+   for (int i = 0; i < num_of_iter; ++i)
     X[i] = x0 + i * h;
-    Y[i] = func_vector[0].Eval(&X[i]);
+  for (auto &func : func_vector)
+  {
+    std::vector<double> Y(num_of_iter);
+    
+    for (int i = 0; i < num_of_iter; ++i)
+      Y[i] = func.Eval(&X[i]);
+    plt::plot(X, Y);
   }
-  plt::named_plot("function 1", X, Y);
+
   plt::show();
 }
--- src/mathlib/mathlib.h
+++ src/mathlib/mathlib.h
@@ -8,13 +8,20 @@
 
 namespace plt = matplotlibcpp;
 
+
+namespace optf{
+  double StronginMethod(FunctionParser& parser, double x0, double x1, double L);
+
+};
+
 class FunctionContainer
 {
 private:
   std::vector<FunctionParser> func_vector;
+  std::vector<std::pair<double,double>> range_vec;
   int operation_id;
 public:
-  FunctionContainer(const std::vector<std::string> &str_func_vector, const std::string &str_arg);
+  FunctionContainer(const std::vector<std::string>& str_func_vector, const std::string& str_arg, std::vector<std::pair<double,double>>& range);
   void Convolution(
     //const std::vector<float> &conv_arg
   );
