67a2ba022bd1181db14fcad85114e7dde659f421
381706-2GuschinAlexander <igushin1999@gmail.com>
2021-11-23 13:35:26 +0300

    change names

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -18,36 +18,36 @@ public:
     int start_y = 35;
     int h_y = 35;
 
-    entry_vec = std::vector<gtk::Entry>(num_of_entry);
+    function_entry_vec = std::vector<gtk::Entry>(num_of_entry);
     coef_entry_vec = std::vector<gtk::Entry>(num_of_entry);
     const auto la = [&](){
       if(last_entry+1 < num_of_entry)
       {
-        entry_vec[++last_entry].show();
+        function_entry_vec[++last_entry].show();
         coef_entry_vec[last_entry].show();
       }
     };
 
     for(int i = 0; i < num_of_entry; ++i){
-      entry_vec[i].signal_activate().connect(la);
-      entry_vec[i].set_size_request(250,30);
+      function_entry_vec[i].signal_activate().connect(la);
+      function_entry_vec[i].set_size_request(250,30);
       fixed.add(coef_entry_vec[i]);
-      fixed.add(entry_vec[i]);
-      fixed.move(entry_vec[i], 20, start_y + i * h_y);
+      fixed.add(function_entry_vec[i]);
+      fixed.move(function_entry_vec[i], 20, start_y + i * h_y);
       fixed.move(coef_entry_vec[i], 300, start_y + i * h_y);
     }
 
-    functions.set_label("f(x)");
-    fixed.add(functions);
-    fixed.move(functions, 125, 10);
+    functions_label.set_label("f(x)");
+    fixed.add(functions_label);
+    fixed.move(functions_label, 125, 10);
 
-    params.set_label("Lambda");
-    fixed.add(params);
-    fixed.move(params, 355, 10);
+    params_label.set_label("Lambda");
+    fixed.add(params_label);
+    fixed.move(params_label, 355, 10);
 
-    range_str.set_label("Range");
-    fixed.add(range_str);
-    fixed.move(range_str, 665, 10);
+    range_str_label.set_label("Range");
+    fixed.add(range_str_label);
+    fixed.move(range_str_label, 665, 10);
 
     
     range = std::vector<gtk::Entry>(2);
@@ -70,8 +70,8 @@ public:
       double x0 = std::stod(range[0].get_text()), x1 = std::stod(range[1].get_text());
       func_range.push_back(std::make_pair(x0,x1));
 
-      for(int i = 0; i < entry_vec.size(); ++i){
-        std::string tmp_string(entry_vec[i].get_text());
+      for(int i = 0; i < function_entry_vec.size(); ++i){
+        std::string tmp_string(function_entry_vec[i].get_text());
         std::string coef(coef_entry_vec[i].get_text());
 
         if(tmp_string != "" && coef != ""){
@@ -84,25 +84,25 @@ public:
       FunctionContainer MO_method(func_vec, str_arg, func_range);
       auto res = MO_method.Convolution(coef_vec, 1e-3);
 
-      num_of_iter.set_label("Number of iteration: " + std::to_string(res.num_iteration));
-      point.set_label("Min at x: " + std::to_string(res.return_point[0]));
-      value.set_label("Function value: " + std::to_string(res.value));
+      num_of_iter_label.set_label("Number of iteration: " + std::to_string(res.num_iteration));
+      point_label.set_label("Min at x: " + std::to_string(res.return_point[0]));
+      value_label.set_label("Function value: " + std::to_string(res.value));
 
       MO_method.DrawPlot(coef_vec, 1e-3, true ,"Conv");
       return true;
       });
 
-    point.set_label("Min at x: ");
-    fixed.add(point);
-    fixed.move(point, 600, 380);
+    point_label.set_label("Min at x: ");
+    fixed.add(point_label);
+    fixed.move(point_label, 600, 380);
 
-    value.set_label("Function value: ");
-    fixed.add(value);
-    fixed.move(value, 600, 415);
+    value_label.set_label("Function value_label: ");
+    fixed.add(value_label);
+    fixed.move(value_label, 600, 415);
 
-    num_of_iter.set_label("Number of iteration: ");
-    fixed.add(num_of_iter);
-    fixed.move(num_of_iter, 600, 450);
+    num_of_iter_label.set_label("Number of iteration: ");
+    fixed.add(num_of_iter_label);
+    fixed.move(num_of_iter_label, 600, 450);
 
     fixed.add(button1);
     fixed.move(button1, 20, 450);
@@ -110,11 +110,11 @@ public:
     set_title("Program");
     resize(800, 500);
     show_all();
-    for(auto& c : entry_vec)
+    for(auto& c : function_entry_vec)
       c.hide();
     for(auto& c : coef_entry_vec)
       c.hide();
-    entry_vec[0].show();
+    function_entry_vec[0].show();
     coef_entry_vec[0].show();
   }
   
@@ -122,15 +122,15 @@ private:
   gtk::Fixed fixed;
   gtk::ScrolledWindow scrolledWindow;
   gtk::Button button1;
-  std::vector<gtk::Entry> entry_vec;
+  std::vector<gtk::Entry> function_entry_vec;
   std::vector<gtk::Entry> coef_entry_vec;
   std::vector<gtk::Entry> range;
-  gtk::Label num_of_iter;
-  gtk::Label point;
-  gtk::Label value;
-  gtk::Label functions;
-  gtk::Label params;
-  gtk::Label range_str;
+  gtk::Label num_of_iter_label;
+  gtk::Label point_label;
+  gtk::Label value_label;
+  gtk::Label functions_label;
+  gtk::Label params_label;
+  gtk::Label range_str_label;
   int num_of_entry = 10;
   int last_entry = 0;
   int button1Clicked = 0;
